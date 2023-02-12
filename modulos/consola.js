4dd7845ff5219f7e580692dba90debf8d41dea04
germanrogu <german.rodriguezgu@gmail.com>
2023-02-12 18:47:07 -0500

    consola

---
+++ modulos/consola.js
@@ -0,0 +1,8 @@
+console.group("Conversacion");
+console.log("Algo");
+console.info("algo");
+console.groupEnd("Conversacion");
+console.error("Algo");
+console.warn("Algo");
+console.table([]);
+console.count("veces");
