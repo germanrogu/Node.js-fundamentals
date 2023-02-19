2f60effa65c90a37fbb754b4415cceaebe552262
germanrogu <german.rodriguezgu@gmail.com>
2023-02-18 19:19:45 -0500

    Modulos propios

---
+++ paquetes/modulos/es6/index.mjs
@@ -0,0 +1,4 @@
+import modulo from "./modulo.mjs";
+//modules experimental node --experimental-modules ...
+
+console.log(modulo());
---
+++ paquetes/modulos/index.js
@@ -0,0 +1,4 @@
+//Traer nuestro modulo y ejecutar una funcion del modulo
+const modulo = require("./modulo");
+
+console.log(modulo);
